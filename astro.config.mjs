6f6d5a578ef6f96e1c41f6821bd77d118bd02af9
BrandonMCoffey <bmc939@gmail.com>
2025-07-23 22:14:00 -0500

    fix url for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://brandoncoffey.com',
+    site: 'brandonmcoffey.github.io',
+    base: '/CoffeyCookbook',
     integrations: [react(), tailwind(), sitemap(), mdx()],
 });
