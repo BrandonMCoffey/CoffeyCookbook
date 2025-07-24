9cc9c03613cdf285517617d9bc5393ba35a90f18
BrandonMCoffey <bmc939@gmail.com>
2025-07-23 22:16:13 -0500

    oops part 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'brandonmcoffey.github.io',
+    site: 'https://brandonmcoffey.github.io',
     base: '/CoffeyCookbook',
     integrations: [react(), tailwind(), sitemap(), mdx()],
 });
